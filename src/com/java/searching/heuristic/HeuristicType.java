d696ce1c8f5c76471102149efc07a0d994cf862f
susTuna <hidasetsuna@gmail.com>
2025-05-19 19:46:48 +0700

    refactor: packaged heuristics into separate classes and interface

---
+++ src/com/java/searching/heuristic/HeuristicType.java
@@ -0,0 +1,8 @@
+package com.java.searching.heuristic;
+
+import com.java.searching.State;
+
+public enum HeuristicType {
+    COMPOSITE, // Original heuristic (Distance + Blockers)
+    MANHATTAN  // Manhattan distance heuristic
+}
